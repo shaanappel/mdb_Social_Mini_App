b93744445f491457db0bd904160a53fc1e5f5e11
Shaan Appel <shaan.appel@yahoo.com>
2016-03-10 00:49:59 -0800

    new social screen fully functional :)

--- mdbSocialsApp/mdbSocialsApp/NewSocialViewController.swift
+++ mdbSocialsApp/mdbSocialsApp/NewSocialViewController.swift
@@ -8,31 +8,54 @@
 
 import UIKit
 
-class NewSocialViewController: UIViewController {
+class NewSocialViewController: UIViewController, UITextFieldDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
     @IBOutlet weak var titleText: UITextField!
     
+    
     @IBOutlet weak var location: UITextField!
 
-    @IBOutlet weak var time: UITextField!
     
+    @IBOutlet weak var socialDescription: UITextField!
     
     @IBOutlet weak var date: UITextField!
     
+    @IBOutlet weak var socialPicture: UIImageView!
     
     @IBAction func dismiss(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
     }
     
+    func displayAlert(title: String, displayError: String) {
+        
+        let alert = UIAlertController(title: title, message: displayError, preferredStyle: UIAlertControllerStyle.Alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: { action in
+            
+        }))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        date.keyboardType = UIKeyboardType.PhonePad
+        date.addTarget(self, action: "textFieldDidChange:", forControlEvents: UIControlEvents.EditingChanged)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func textFieldDidChange(textField: UITextField) {
+        if date.text!.characters.count == 2 {
+            date.text = date.text! + "/"
+        }
+        if date.text!.characters.count == 5 {
+            date.text = date.text! + "/"
+        }
     }
     
     @IBAction func post(sender: AnyObject) {
@@ -41,16 +64,89 @@ class NewSocialViewController: UIViewController {
 //        
 //        database, and then takes the user back to the SocialsFeedViewController.
         
-//        let user = PFUser()
-//        user.titleText = titleText.text
-//        user.location = location.text
-//        user.time = time.text
-//        user.date = date.text
+        
+        let dataString = date.text! as String
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "MM-dd-yyyy"
+        dateFormatter.timeZone = NSTimeZone.localTimeZone()
+        
+        // convert string into date
+        let dateValue = dateFormatter.dateFromString(dataString) as NSDate!
+        
+        
+        
+        
+        var displayError = ""
+        if titleText.text == "" {
+            displayError = "Please enter a Social Event name"
+        } else if socialDescription.text == "" {
+            displayError = "Please enter a description"
+        } else if location.text == "" {
+            displayError = "Please enter a location"
+        }  else if date.text == "" {
+            displayError = "Please enter a date"
+        }  else if dateValue == nil {
+            displayError = "Please enter a valid date"
+        }
+        
+        if displayError != "" {
+            displayAlert("Incomplete Form", displayError: displayError)
+        } else {
+            var newSocial = PFObject(className:"Socials")
+            newSocial["socialTitle"] = titleText.text!
+            newSocial["socialInformation"] = socialDescription.text!
+            newSocial["socialLocation"] = location.text!
+            newSocial["socialTime"] = dateValue
+            
+            
+            
+            
+            let socialPicFile = PFFile(name: "socialPic.jpg", data: UIImageJPEGRepresentation(socialPicture.image!, 0.5)!)
+            
+            newSocial["socialPicture"] = socialPicFile
+            
+            
+            newSocial.saveInBackgroundWithBlock {
+                (success: Bool, error: NSError?) -> Void in
+                if (success) {
+                    // The object has been saved.
+                    print("sucesssss!!!!")
+                    self.dismissViewControllerAnimated(true, completion: nil)
+                } else {
+                    // There was a problem, check error.description
+                    if let theError = error!.userInfo["error"] as? NSString {
+                        displayError = theError as String
+                    } else {
+                        displayError = "Please try again later!"
+                    }
+                    self.displayAlert("Could Not Signup", displayError: displayError)
+                }
+            }
+        }
+
         
     }
 
     @IBAction func upload(sender: AnyObject) {
+        let image = UIImagePickerController()
+        image.delegate = self
+        image.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        image.allowsEditing = false
+        self.presentViewController(image, animated: true, completion: nil)
+        
+        
     }
+    
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+        socialPicture.image = image
+        
+        socialPicture.layer.cornerRadius = socialPicture.frame.size.width/2
+        socialPicture.clipsToBounds = true
+        
+    }
+    
     /*
     // MARK: - Navigation
 
